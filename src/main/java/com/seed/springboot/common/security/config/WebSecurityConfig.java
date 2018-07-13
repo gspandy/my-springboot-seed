5f34d2504cacda7ca39ef2b7a38ea5c466fc6a3f
ruyangit@163.com <ruyangit@163.com>
2018-07-13 13:05:13 +0800

    初始化

---
+++ src/main/java/com/seed/springboot/common/security/config/WebSecurityConfig.java
@@ -0,0 +1,62 @@
+/**   
+ * @Title: WebSecurityConfig.java 
+ * @Package com.seed.springboot.common.security.config 
+ * @version V1.0   
+ */
+package com.seed.springboot.common.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/** 
+ * @ClassName: WebSecurityConfig 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author RuYang ruyang@fosun.com
+ * @date 2018年7月10日 下午6:08:25 
+ *  
+ */
+@Configuration
+//@EnableWebSecurity
+@Order(1)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+    private UserDetailsService userDetailsService;
+	
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(8);
+    }
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+	
+	@Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+        http.requestMatchers().antMatchers(HttpMethod.OPTIONS, "/**")
+                .and()
+                .authorizeRequests().antMatchers("/oauth/**").permitAll()
+                .and()
+                .csrf().disable();
+        // @formatter:on
+	}
+}
